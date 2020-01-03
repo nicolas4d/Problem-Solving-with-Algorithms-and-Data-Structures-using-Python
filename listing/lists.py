90fe1a6a89c9d5a61eff98af17a939cb5b3615b3
nicolas4d <nicolas4d@foxmail.com>
2020-01-03 22:37:02 +0800

    chapter 3 done

---
+++ listing/lists.py
@@ -0,0 +1,57 @@
+from timeit import Timer
+import timeit
+
+def test1():
+    l = []
+    for i in range(1000):
+        l = l + [i]
+
+def test2():
+    l = []
+    for i in range(1000):
+        l.append(i)
+
+def test3():
+    l = [i for i in range(1000)]
+
+def test4():
+    l = list(range(1000))
+
+
+t1 = Timer("test1()", "from __main__ import test1")
+print("concat ",t1.timeit(number=1000), "milliseconds")
+t2 = Timer("test2()", "from __main__ import test2")
+print("append ",t2.timeit(number=1000), "milliseconds")
+t3 = Timer("test3()", "from __main__ import test3")
+print("comprehension ",t3.timeit(number=1000), "milliseconds")
+t4 = Timer("test4()", "from __main__ import test4")
+print("list range ",t4.timeit(number=1000), "milliseconds")
+
+print()
+
+popzero = timeit.Timer("x.pop(0)",
+                       "from __main__ import x")
+popend = timeit.Timer("x.pop()",
+                      "from __main__ import x")
+
+x = list(range(2000000))
+print(popzero.timeit(number=1000))
+4.8213560581207275
+
+x = list(range(2000000))
+print(popend.timeit(number=1000))
+
+print()
+
+popzero = Timer("x.pop(0)",
+                "from __main__ import x")
+popend = Timer("x.pop()",
+               "from __main__ import x")
+print("pop(0)   pop()")
+for i in range(1000000,100000001,1000000):
+    x = list(range(i))
+    pt = popend.timeit(number=1000)
+    x = list(range(i))
+    pz = popzero.timeit(number=1000)
+    print("%15.5f, %15.5f" %(pz,pt))
+
---
+++ listing/sumOf2.py
@@ -0,0 +1,23 @@
+import time
+
+def sumOfN2(n):
+   start = time.time()
+
+   theSum = 0
+   for i in range(1,n+1):
+      theSum = theSum + i
+
+   end = time.time()
+
+   return theSum, end - start
+
+print(sumOfN2(10))
+
+for i in range(5):
+       print("Sum is %d required %10.7f seconds" % sumOfN2(10000))
+
+for i in range(5):
+       print("Sum is %d required %10.7f seconds" % sumOfN2(100000))
+
+for i in range(5):
+       print("Sum is %d required %10.7f seconds"% sumOfN2(1000000))
