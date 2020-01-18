8768892e603f48c5159b52ac6a40fffa2426ee47
nicolas4d <nicolas4d@foxmail.com>
2020-01-19 00:19:14 +0800

    chapter 6 Discussion Questions 1

--- code/shortBubbleSort.py
+++ code/shortBubbleSort.py
@@ -8,6 +8,7 @@ def shortBubbleSort(alist):
         for i in range(passnum) :
             if alist[i] > alist[i + 1] :
                 exchages = True
+                
                 temp = alist[i]
                 alist[i] = alist[i + 1]
                 alist[i + 1] = temp
